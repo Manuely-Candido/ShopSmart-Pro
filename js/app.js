7a502426324c4d5636e8911a47895b4f093265fe
Manuely <manuely.candido@aluno.senai.br>
2026-02-20 08:53:03 -0300

    Melhorando e organizando algumas frases e variaveis

--- js/app.js
+++ js/app.js
@@ -18,7 +18,7 @@ function calcularTotal(){
         resultado.innerHTML = `   
         <h2>Total da Compra</h2>
         <ul>
-            <li>O valor total da sua compra será de <span>R$ ${v1 * v2.toFixed(2)}</span> </li>
+            <li>O valor total da sua compra será de <span>R$ ${(v1 * v2).toFixed(2)}</span> </li>
         </ul>
         `
     };
@@ -29,6 +29,7 @@ function desconto(){
     const v1 = parseFloat(input01.value);
     const v2 = parseFloat(input02.value);
     const desconto = (v1 * v2)/100
+    const total = v1 - desconto
 
     if(isNaN(v1)  || isNaN(v2)) {
         resultado.style.display = 'flex';
@@ -44,7 +45,7 @@ function desconto(){
             <ul>    
                 <li>Foi aplicado um desconto de ${v2}% sobre uma compra de R$ ${v1.toFixed(2)}. </li>
                 <li>O valor do desconto será de <span>R$ ${desconto.toFixed(2)}</span> </li>
-                <li>E o valor total da compra com o desconto é <span>R$ ${v1 - desconto.toFixed(2)}</span> </li>
+                <li>E o valor total descontado é <span>R$ ${total.toFixed(2)}</span> </li>
             </ul>
         `
     };
@@ -55,6 +56,7 @@ function juros(){
     const v1 = parseFloat(input01.value);
     const v2 = parseFloat(input02.value);
     const acrescimo = (v1 * v2)/100
+    const total = v1 + acrescimo
 
     
 
@@ -70,8 +72,8 @@ function juros(){
         resultado.innerHTML = `
             <h2>Juros</h2>    
             <ul> 
-                <li>O acréscimo será de ${v2}% sobre o valor de R$${v1.toFixed(2)} = <span>R$ ${acrescimo.toFixed(2)}</span> </li>
-                <li>O valor total com o acréscimo é <span>R$ ${v1 + acrescimo.toFixed(2)}</span> </li>
+                <li>O acréscimo será de ${v2}% sobre o valor de R$${v1.toFixed(2)}</span> </li>
+                <li>O valor total com o acréscimo será de <span>R$ ${total.toFixed(2)}</span> </li>
             </ul>
         `
     };
